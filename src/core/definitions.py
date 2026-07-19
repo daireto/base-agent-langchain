256973261f94aa46201b7b267bc146fc30eaff94
Dairo Mosquera <dairoandres115@outlook.com>
2026-07-18 21:36:26 -0500

    feat(core): add initial definitions for database paths
    Introduced a new definitions module containing constants for various database paths and configurations. This includes paths for checkpoints, conversations, Chroma, QDrant, and prompt templates, as well as the LangChain API version and default user ID. These definitions will facilitate easier management and future updates to database configurations.

---
+++ src/core/definitions.py
@@ -0,0 +1,19 @@
+CHECKPOINTER_PATH = '.checkpoints.sqlite'  # TODO: Replace by postgres table
+
+CONVERSATIONS_DB_PATH = '.conversations.sqlite'  # TODO: Replace by postgres table
+
+# Chroma
+CHROMA_DB_DIR = '.chroma_db'  # TODO: Replace by postgres table
+
+# QDrant
+QDRANT_DB_DIR = '.qdrant_db'  # TODO: Replace by postgres table
+QDRANT_UEFA_DOCS_COLLECTION = 'uefa_docs'
+
+# Prompts
+PROMPT_TEMPLATE_DIR = 'prompts'
+
+# LangChain
+LANGCHAIN_API_VERSION = 'v2'
+
+# Defaults
+DEFAULT_USER_ID = 'default_user'
