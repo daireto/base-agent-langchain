70cbafd1cea42bf62e21cae613986d81d7225abf
Dairo Mosquera <dairoandres115@outlook.com>
2026-07-25 22:39:56 -0500

    refactor(agents-uefa): restructure UEFA agent initialization
    Refactored the initialization of the UEFA agent by encapsulating the logic within a dedicated function, `get_uefa_agent`. This change improves code organization and readability, making it easier to manage the agent's setup and configuration.

--- src/agents/uefa_agent/agent.py
+++ src/agents/uefa_agent/agent.py
@@ -1,21 +1,20 @@
 from langchain.agents import create_agent
-from langchain.chat_models import init_chat_model
+from langgraph.graph.state import CompiledStateGraph
 
 from agents.uefa_agent.tools import uefa_docs_retriever
 from core.config import settings
 from core.prompt_manager import prompt_manager
 
-model = init_chat_model(
-    model=settings.uefa_agent.model,
-    temperature=settings.uefa_agent.temperature,
-    max_tokens=settings.uefa_agent.max_tokens,
-    timeout=settings.uefa_agent.timeout,
-    max_retries=settings.uefa_agent.max_retries,
-    base_url=settings.uefa_agent.base_url,
-)
 
-uefa_agent = create_agent(
-    model,
-    tools=[uefa_docs_retriever],
-    system_prompt=prompt_manager.get('uefa_agent_prompt'),
-)
+def get_uefa_agent() -> CompiledStateGraph:
+    """Create and return the UEFA agent."""
+    model = settings.uefa_agent.init_chat_model()
+
+    return create_agent(
+        model,
+        tools=[uefa_docs_retriever],
+        system_prompt=prompt_manager.get('uefa_agent_prompt'),
+    )
+
+
+uefa_agent = get_uefa_agent()
