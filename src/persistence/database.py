29270a4362c0c8e8ab4feda89f18fac606eacd00
Dairo Mosquera <dairoandres115@outlook.com>
2026-07-26 01:44:32 -0500

    refactor(persistence): rename database connection function
    Renamed the database connection function from `get_db_connection` to `init_database` for better clarity and consistency. The function now initializes the database connection and yields it for use, improving the overall readability of the code.

--- src/persistence/database.py
+++ src/persistence/database.py
@@ -6,11 +6,10 @@ from sqlactive import DBConnection
 from core.config import settings
 from persistence.models import BaseModel
 
-db = DBConnection(settings.database.url.get_secret_value())
-
 
 @asynccontextmanager
-async def get_db_connection() -> AsyncGenerator[DBConnection]:
+async def init_database() -> AsyncGenerator[DBConnection]:
+    db = DBConnection(settings.database.url.get_secret_value())
     await db.init_db(BaseModel)
     yield db
     await db.close()
