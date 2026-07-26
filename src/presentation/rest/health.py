ff4a36f911a3630540f4b4b2fb19cd64bc4afda9
Dairo Mosquera <dairoandres115@outlook.com>
2026-07-26 01:46:37 -0500

    refactor(rest): enhance documentation for server health
    Improved docstrings for the ServerHealth class and its methods to provide clearer explanations of their functionality. This change aims to enhance code readability and maintainability by ensuring that the purpose of each method is well-documented.

--- src/presentation/rest/health.py
+++ src/presentation/rest/health.py
@@ -2,26 +2,33 @@ from functools import lru_cache
 
 
 class ServerHealth:
+    """Handles the health status of the server."""
+
     def __init__(self) -> None:
         self.reset()
 
     @property
     def is_healthy(self) -> bool:
+        """Whether the server is currently healthy."""
         return self._is_healthy
 
     @is_healthy.setter
     def is_healthy(self, value: bool) -> None:
+        """Set the health status of the server."""
         self._is_healthy = value
 
     def reset(self) -> None:
+        """Reset the health status to healthy and clear any unhealthy reason."""
         self._is_healthy = True
         self._unhealthy_reason = ''
 
     def set_unhealthy(self, reason: str) -> None:
+        """Set the server's health status to unhealthy with a reason."""
         self._is_healthy = False
         self._unhealthy_reason = reason
 
     def get_unhealthy_reason(self) -> str:
+        """Return the reason for the server being unhealthy."""
         return self._unhealthy_reason
 
     def __str__(self) -> str:
@@ -37,6 +44,7 @@ class ServerHealth:
 
 @lru_cache
 def get_server_health() -> ServerHealth:
+    """Return a singleton instance of ServerHealth."""
     return ServerHealth()
 
 
