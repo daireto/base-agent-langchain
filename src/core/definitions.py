b5c5edc5458ace5685ba1bec0008fd3a451a4ffa
Dairo Mosquera <dairoandres115@outlook.com>
2026-07-25 22:52:30 -0500

    refactor(core): update TODO comments for clarity
    Revised TODO comments in the definitions.py file to specify the intended replacements for database implementations. Clarified the transition from placeholder SQLite paths to the respective services for Chroma and Qdrant, enhancing the understanding of future development tasks.

--- src/core/definitions.py
+++ src/core/definitions.py
@@ -1,10 +1,11 @@
-CHECKPOINTER_PATH = '.checkpoints.sqlite'  # TODO: Replace by postgres table
+CHECKPOINTER_PATH = '.checkpoints.sqlite'  # TODO: Replace by postgres DB
 
 # Chroma
-CHROMA_DB_DIR = '.chroma_db'  # TODO: Replace by postgres table
+CHROMA_DB_DIR = '.chroma_db'  # TODO: Replace by Chroma service
+CHROMA_MEMORIES_COLLECTION = 'memories'
 
 # QDrant
-QDRANT_DB_DIR = '.qdrant_db'  # TODO: Replace by postgres table
+QDRANT_DB_DIR = '.qdrant_db'  # TODO: Replace by Qdrant service
 QDRANT_UEFA_DOCS_COLLECTION = 'uefa_docs'
 
 # Prompts
