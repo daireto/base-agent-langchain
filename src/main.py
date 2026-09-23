b6498783d7a2e23fb7a832da62fb50b6859a389b
Dairo Mosquera <dairoandres115@outlook.com>
2026-09-23 16:32:30 -0500

    refactor(main): improve logging and presentation mode handling
    Updated the main application logic to enhance logging by replacing print statements with logger exceptions. Adjusted the presentation mode checks to ensure proper application initialization for both API and UI modes. This change improves error handling and maintains consistency in the application's logging practices.

--- src/main.py
+++ src/main.py
@@ -1,8 +1,11 @@
 from agents.uefa_agent.store import uefa_store
 from core.config import settings
+from core.logger import get_logger
 from presentation.api.app import create_default_app
 
-if settings.presentation_mode == 'rest':
+_logger = get_logger('main')
+
+if settings.presentation_mode == 'api':
     app = create_default_app()
 
 if __name__ == '__main__':
@@ -12,7 +15,7 @@ if __name__ == '__main__':
         try:
             from streamlit.web import cli as st_cli
         except ImportError:
-            print('Streamlit no está instalado.')
+            _logger.exception('Streamlit no está instalado.')
             sys.exit(1)
 
         try:
@@ -24,11 +27,18 @@ if __name__ == '__main__':
     elif settings.presentation_mode == 'api':
         import uvicorn
 
+        reload_ = settings.rest_server.is_dev
+        if settings.qdrant.mode == 'local':
+            # Disable auto-reload when using a local Qdrant database
+            # to avoid issues related to qdrant folder being accessed
+            # by multiple instances in different threads.
+            reload_ = False
+
         uvicorn.run(
             'main:app',
             host='0.0.0.0',
             port=settings.rest_server.port,
             log_config=None,
-            # reload=settings.rest_server.is_dev,
+            reload=reload_,
             forwarded_allow_ips='*' if settings.rest_server.behind_proxy else None,
         )
